21cf40744d7733ed0d7a7d226ae48cc73a04ec66
Ralph Bacon <arduino@jargongeneration.uk>
2019-03-11 18:19:51 +0000

    Secret stuff goes in here and rename to config.h

---
+++ ConfigDemo.h
@@ -0,0 +1,17 @@
+/*
+ * config.h
+ *
+ *  Created on: 10 Mar 2019
+ *      Author: Ralph
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define ThingSpeakWriteApiKey	"3PDRNBD973HJ572E"
+#define ThingSpeakReadApiKey	"ZMKLIUV2QFZIY223"
+
+#define _ssid 		"RalphWiFi";
+#define _password 	"Arduino";
+
+#endif /* CONFIG_H_ */
