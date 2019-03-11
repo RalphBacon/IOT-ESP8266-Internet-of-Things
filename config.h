fcf2028869705c6eb8a889e4e04d1d6f7388488b
Ralph Bacon <arduino@jargongeneration.uk>
2019-03-11 18:19:02 +0000

    Put your secret stuff in here (and rename it to config.h)

---
+++ config.h
@@ -0,0 +1,17 @@
+/*
+ * config.h
+ *
+ *  Created on: 10 Mar 2019
+ *      Author: Ralph
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define ThingSpeakWriteApiKey	"3PDRNBW89IYM572E"
+#define ThingSpeakReadApiKey	"ZMHUTVH6QFZIY223"
+
+#define _ssid 		"Greypuss";
+#define _password 	"mrSeal89";
+
+#endif /* CONFIG_H_ */
